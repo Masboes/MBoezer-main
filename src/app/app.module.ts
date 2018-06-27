ab7b38beee6db179e7b923700999c0daef4c2b19
Mathijs B <mathijs.boezer@gmail.com>
2018-06-27 14:17:57 +0200

    Add router, add first page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ToolsPageComponent } from './tools-page/tools-page.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'tools', pathMatch: 'full'},
+  { path: 'tools', component: ToolsPageComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    ToolsPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {useHash: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
