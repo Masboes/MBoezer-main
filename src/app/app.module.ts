fb0c0daca2d692618a6db4aaac6a24129a886479
Mathijs B <mathijs.boezer@gmail.com>
2018-06-27 13:58:45 +0200

    Repair layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
