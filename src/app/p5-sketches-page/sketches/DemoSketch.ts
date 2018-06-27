27fe7b7a5bf9bd81b31218765e19ffbac58b4d20
Mathijs B <mathijs.boezer@gmail.com>
2018-06-27 15:51:13 +0200

    Create demo sketch

---
+++ src/app/p5-sketches-page/sketches/DemoSketch.ts
@@ -0,0 +1,23 @@
+import {Sketch} from "./Sketch";
+
+export class DemoSketch extends Sketch {
+  public name: string = 'Demo Sketch';
+
+  protected setup(p: any): () => void {
+    return () => {
+      console.log('setup');
+    }
+  }
+
+  protected draw(p: any): () => void {
+    return () => {
+      console.log(p.mouseIsPressed);
+      if (p.mouseIsPressed) {
+        p.fill(0);
+      } else {
+        p.fill(255);
+      }
+      p.ellipse(p.mouseX, p.mouseY, 80, 80);
+    }
+  }
+}
