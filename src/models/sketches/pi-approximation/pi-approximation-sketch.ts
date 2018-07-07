68519aeb39320e1dcfcbdde2da950022423bab0d
Mathijs B <mathijs.boezer@gmail.com>
2018-07-07 09:07:17 +0200

    Implement pi approximation sketch

--- src/models/sketches/pi-approximation/pi-approximation-sketch.ts
+++ src/models/sketches/pi-approximation/pi-approximation-sketch.ts
@@ -1,6 +1,7 @@
 import {Sketch} from "../sketch";
 import {FormFactory} from "../../form/form-factory";
 import {Form} from "../../form/form";
+import {SketchVector} from "../sketch-vector";
 
 export class PiApproximationSketch extends Sketch {
   public sketchName: string = 'pi-approximation';
@@ -9,6 +10,14 @@ export class PiApproximationSketch extends Sketch {
   public sketchDescription: string = 'A visual way to approximate Pi.';
 
   private paused: boolean = false;
+  private pointRadius: number = 2;
+  private pointsPerDraw: number = 10;
+
+  private radius: number;
+  private origin: SketchVector = {x: 0, y: 0};
+
+  private totalPoints: number = 0;
+  private inCirclePoints: number = 0;
 
   public getSettingsForm(formFactory: FormFactory): Form {
     return formFactory.createFormBuilder()
@@ -22,13 +31,67 @@ export class PiApproximationSketch extends Sketch {
 
   protected setup(p: any): () => void {
     return () => {
+      this.radius = Math.min(p.height, p.width) / 2 * 0.8;
 
+      p.translate(p.width / 2, p.height / 2);
+      p.background(255);
+      p.textSize(30);
+      p.fill(0);
+      p.noStroke();
+      let textOffset = Math.min(p.height, p.width) / 2 * 0.90;
+      p.text('ΔCircle / ΔRectangle = πr²/4r² => π ≈ 4 x ΔCircle / ΔRectangle', this.origin.x - textOffset, this.origin.y - textOffset);
     }
   }
 
   protected draw(p: any): () => void {
     return () => {
+      p.translate(p.width / 2, p.height / 2);
 
+      this.drawCircleRect(p);
+
+      if(!this.paused) {
+        for(let i = 0; i < this.pointsPerDraw; i++) {
+          this.addPoint(p);
+        }
+      }
+
+      this.updateApproxPi(p);
     }
+  }
+
+  private addPoint(p: any): void {
+    let color = {r: 0, g: 0, b: 255};
+    let point = {
+      x: (Math.random() * 2 - 1) * this.radius + this.origin.x,
+      y: (Math.random() * 2 - 1) * this.radius + this.origin.y,
+    };
+    this.totalPoints++;
+    if(((point.x - this.origin.x)**2 + (point.y - this.origin.y)**2)**0.5 <= this.radius) { // if inside circle
+      this.inCirclePoints++;
+      color.r = 255;
+      color.b = 0;
+    }
+
+    p.noStroke();
+    p.fill(color.r, color.g, color.b);
+    p.ellipse(point.x, point.y, this.pointRadius * 2, this.pointRadius * 2);
+  }
+
+  private updateApproxPi(p: any): void {
+    let textOffset = Math.min(p.height, p.width) / 2 * 0.90;
+    p.noStroke();
+    p.fill(255);
+    p.rect(this.origin.x - this.radius, this.origin.y + this.radius + 1, this.radius * 2, this.radius); // clear pi approx area
+    p.textSize(30);
+    p.fill(0);
+    p.text('π ≈ ' + 4 * this.inCirclePoints / this.totalPoints, this.origin.x - this.radius, this.origin.y + textOffset);
+  }
+
+  private drawCircleRect(p: any): void {
+    p.stroke(0);
+    p.fill('rgba(255,255,255,0.01)');
+    p.rect(this.origin.x - this.radius, this.origin.y - this.radius, this.radius * 2, this.radius * 2);
+    p.noFill();
+    p.ellipse(this.origin.x, this.origin.y, this.radius * 2, this.radius * 2);
   }
 }
